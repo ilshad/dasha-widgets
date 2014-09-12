5e5a524d1b98e4ec905656b4f50bc033c0055001
Ilshad Khabibullin <astoon.net@gmail.com>
2014-09-12 14:32:54 +0400

    init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dasha-widgets
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/dasha_widgets/core_test.clj
@@ -0,0 +1,7 @@
+(ns dasha-widgets.core-test
+  (:require [clojure.test :refer :all]
+            [dasha-widgets.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
